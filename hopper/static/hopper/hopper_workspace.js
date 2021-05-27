e40306a1c9c0a1a0525550be80abe04bb478bc86
kuzichkin2001 <kuzichkin.p@yandex.ru>
2021-05-27 22:03:06 +0400

    hopper started to move, it's awesome :)

--- hopper/static/hopper/hopper_workspace.js
+++ hopper/static/hopper/hopper_workspace.js
@@ -32,32 +32,6 @@ canvas.height = canvas.scrollHeight;
 
 const hopper = new Image();
 
-var currentPosition = {
-    x: 15,
-    y: 190,
-};
-
-const commands = parseGrasshopperCode();
-canvas.onclick = () => {
-    for (let command of commands) {
-        let ctx = {
-            command,
-            currentPosition
-        };
-        if (command.direction === 'left') {
-            console.log('left');
-            for (let iter = 0; iter < command.value; ++iter) {
-                renderCanvas.call(ctx);
-            }
-        } else {
-            console.log('right');
-            for (let iter = 0; iter < command.value; ++iter) {
-                renderCanvas.call(ctx);
-            }
-        }
-    }
-}
-
 function renderWorkSpace() {
     context.beginPath();
     context.moveTo(40, 250);
@@ -73,35 +47,187 @@ function renderWorkSpace() {
 const duration = 250;
 const step_x = 70;
 const step_y = 50;
-let restrict_x = 0;
+const currentPosition = {
+    x: 15,
+    y: 190,
+};
 
 renderWorkSpace();
 
-function renderCanvas() {
-    requestAnimationFrame(renderCanvas.bind(this));
-    if (this.direction === 'left') {
-        console.log("захожу влево");
-        if (restrict_x < 70) {
-            this.currentPosition.x -= step_x / duration;
-            this.currentPosition.y -= (restrict_x <= 35 ? 1 : -1) * step_y / duration;
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            restrict_x += step_x / duration;
-            renderWorkSpace();
-            context.drawImage(hopper, currentPosition.x, currentPosition.y, 50, 50);
-        } else {
-            restrict_x = 0;
-        }
-    } else {
-        console.log("захожу вправо");
-        this.currentPosition.x += step_x / duration;
-        this.currentPosition.y -= (restrict_x <= 35 ? 1 : -1) * step_y / duration;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        renderWorkSpace();
-        context.drawImage(hopper, currentPosition.x, currentPosition.y, 50, 50);
+const debounce = (func, wait) => {
+  let interval;
+
+  return function executedFunction(...args) {
+  const later = () => {
+      clearInterval(interval);
+      func(...args);
+    };
+    clearInterval(interval);
+    interval = setInterval(later, wait);
+  };
+};
+
+function jump(obj) {
+    const { clear, update, render } = obj;
+    let pTimestamp = 0;
+
+    requestAnimationFrame(tick);
+
+    function tick(timestamp) {
+        let myRequest = requestAnimationFrame(tick);
+        const diff = timestamp - pTimestamp;
+        pTimestamp = timestamp;
+        const fps = 1000 / diff;
+        const secondPart = diff / 1000;
+
+        const params = {
+            pTimestamp,
+            timestamp,
+            diff,
+            fps,
+            secondPart,
+            myRequest,
+        };
+
+        update(params);
+        clear();
+        render(params);
     }
-    restrict_x = 0;
 }
 
+canvas.onclick = function() {
+    currentPosition.x = 15;
+    currentPosition.y = 190;
+    const commands = parseGrasshopperCode();
+    let angle = 0;
+    const right = debounce((iter) => {
+        console.log(iter);
+        var interval = setInterval(() => jump({
+            clear() {
+                context.clearRect(0, 0, canvas.width, canvas.height);
+            },
+            update(params) {
+                currentPosition.x = (15 - iter * 70) + 35 * (Math.cos(angle) - 1);
+                currentPosition.y = 190 - 15 * Math.sin(angle);
+                angle += 0.09;
+                if (angle > Math.PI) {
+                    angle = 0;
+                    iter++;
+                    cancelAnimationFrame(params.myRequest);
+                    clearInterval(interval);
+                }
+            },
+            render() {
+                renderWorkSpace();
+                context.drawImage(hopper, currentPosition.x, currentPosition.y, 50, 50);
+            }
+        }), 250);
+    }, 250);
+    const left = debounce((iter) => {
+        console.log(iter);
+        var interval = setInterval(() => jump({
+            clear() {
+                context.clearRect(0, 0, canvas.width, canvas.height);
+            },
+            update(params) {
+                currentPosition.x = (15 - iter * 70) + 35 * (Math.cos(angle) - 1);
+                currentPosition.y = 190 - 15 * Math.sin(angle);
+                angle += 0.09;
+                if (angle > Math.PI) {
+                    angle = 0;
+                    iter++;
+                    cancelAnimationFrame(params.myRequest);
+                    clearInterval(interval);
+                }
+            },
+            render() {
+                renderWorkSpace();
+                context.drawImage(hopper, currentPosition.x, currentPosition.y, 50, 50);
+            }
+        }), 250);
+
+    }, 250);
+    for (let command of commands) {
+        let iter = 0;
+        if (command.direction === 'right') {
+            for (let i = 0; i < command.value; ++i) {
+                setTimeout(() => right(i), 250 * (iter + 2));
+                iter++;
+            }
+        } else if (command.direction === 'left') {
+            for (let i = 0; i < command.value; ++i) {
+                setTimeout(() => left(i), 250 * (iter + 2));
+                iter++;
+            }
+        }
+    }
+
+
+    // for (let command of commands) {
+    //     let iter = 0;
+    //     let angle = 0;
+    //     renderCanvas({
+    //         clear() {
+    //             context.clearRect(0, 0, canvas.width, canvas.height);
+    //         },
+    //         update(params) {
+    //             if (command.direction === 'left') {
+    //                 console.log('left');
+    //                 currentPosition.x = Math.abs(35 * Math.cos(angle));
+    //                 if (angle > Math.PI) {
+    //                     iter++;
+    //                 }
+    //                 if (command.value === iter) {
+    //                     cancelAnimationFrame(params.myRequest);
+    //                 }
+    //             } else if (command.direction === 'right') {
+    //                 console.log('right');
+    //                 currentPosition.x = Math.abs(35 * Math.cos(Math.PI - angle));
+    //                 if (angle > Math.PI) {
+    //                     iter++;
+    //                 }
+    //                 if (command.value === iter) {
+    //                     cancelAnimationFrame(params.myRequest);
+    //                 }
+    //             }
+    //             angle += 0.18;
+    //             currentPosition.y = 190 - 15 * Math.sin(angle);
+    //         },
+    //         render() {
+    //             renderWorkSpace();
+    //             context.drawImage(hopper, currentPosition.x, currentPosition.y, 50, 50);
+    //         },
+    //     });
+    // }
+}
+
+// function renderCanvas(obj) {
+//     const { clear, update, render } = obj;
+//     let pTimestamp = 0;
+//
+//     requestAnimationFrame(tick);
+//
+//     function tick(timestamp) {
+//         let myRequest = requestAnimationFrame(tick);
+//         const diff = timestamp - pTimestamp;
+//         pTimestamp = timestamp;
+//         const fps = 1000 / diff;
+//         const secondPart = diff / 1000;
+//
+//         const params = {
+//             pTimestamp,
+//             timestamp,
+//             diff,
+//             fps,
+//             secondPart,
+//             myRequest,
+//         };
+//
+//         update(params);
+//         clear();
+//         render(params);
+//     }
+// }
 
 hopper.src = '/static/hopper/images/hopper.png';
 
